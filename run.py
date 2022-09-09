fe5bab36c0c1fc2abc61339d0d3bc97a57b594bd
Johan Kiviniemi <devel@johan.kiviniemi.name>
2022-09-10 00:54:29 +0300

    run.py: Use argparse

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 
 from overviewer_quotes_word_cloud import OverviewerQuotes, render
@@ -6,9 +7,24 @@ from overviewer_quotes_word_cloud import OverviewerQuotes, render
 def main():
     logging.basicConfig(level=logging.INFO)
 
+    ap = argparse.ArgumentParser(description="#overviewer quotes word cloud generator")
+    ap.add_argument(
+        "input",
+        metavar="INPUT_PATH",
+        type=str,
+        help="quote database dump filename",
+    )
+    ap.add_argument(
+        "output",
+        metavar="OUTPUT_PATH",
+        type=str,
+        help="image filename",
+    )
+    args = ap.parse_args()
+
     ovq = OverviewerQuotes()
-    ovq.read("20220908_IRCQuotes.cleaned-up.txt")
-    render("overviewer_quotes_word_cloud.png", ovq.quotes)
+    ovq.read(args.input)
+    render(args.output, ovq.quotes)
 
 
 if __name__ == "__main__":
